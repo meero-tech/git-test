2d24b8edfd33283dc56e834c9986fe9a7654c4dc
meero-tech <miraraudhatuljannah98@gmail.com>
2020-10-02 20:21:56 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, aperiam minus soluta nostrum vitae minima officia molestiae deserunt, quod quasi facere labore optio error iusto quo sunt reiciendis molestias? Nobis.</p>
 </body>
 </html>
